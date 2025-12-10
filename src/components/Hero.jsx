bf1c6d89587951790674824b43047c471c452b82
Fadwa <fadwazejli58@gmail.com>
2025-12-10 17:15:29 +0100

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,9 @@ export default function Hero(){
 return (
 <section className="text-center mt-10">
 <h2 className="text-4xl md:text-5xl font-bold mb-4">• Développeur Web •</h2>
-<p className="text-slate-300 max-w-2xl mx-auto">Étudiant 42 Paris — Je conçois des interfaces soignées, performantes et maintenables.</p>
+<p className="text-slate-300 max-w-2xl mx-auto">Étudiante 42 Paris</p>
 </section>
 )
+const bio = `Étudiant à 42 Paris, passionné par le développement web et la 3D, spécialisé en React, TypeScript et Tailwind.
+Je crée des projets esthétiques, performants et immersifs, et je cherche toujours à apprendre et à innover.`;
 }
